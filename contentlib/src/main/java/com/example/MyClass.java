c50f3f1933e6ee30472d6b0154bb534f63deb590
snow <cyp206@qq.com>
2017-11-15 19:27:33 +0800

    提莫会放毒

--- contentlib/src/main/java/com/example/MyClass.java
+++ contentlib/src/main/java/com/example/MyClass.java
@@ -1,6 +1,9 @@
 package com.example;
 
+import com.example.sort.FindDuplicates;
 import com.example.sort.QuickSort;
+
+import java.util.List;
 
 public class MyClass {
 
@@ -8,8 +11,27 @@ public class MyClass {
         System.out.println("___");
 
 
-        QuickSort quickSort = new QuickSort();
-        quickSort.sort(new int[]{5,1,2,3,6,7,8,8,0});
+//        QuickSort quickSort = new QuickSort();
+//        quickSort.sort(new int[]{5,1,2,3,6,7,8,8,0});
+        int[] ints = {5,2,2,3,5};
+//        int[] ints = {5, 1, 2, 3, 6, 7, 5, 1, 2};
+        System.out.println( "{5, 1, 2, 3, 6, 7, 5, 1, 2}");
+
+        List<Integer> duplicates = FindDuplicates.findDuplicates(ints);
+      System.out.print(  duplicates.toString());
     }
 
+
+    /**
+     *
+     *
+     * 5,2,2,3,5
+     *         -5
+     *   -2
+     *   2              2
+     *      -2
+     *
+     *
+     *
+     */
 }
---
+++ contentlib/src/main/java/com/example/sort/FindDuplicates.java
@@ -0,0 +1,36 @@
+package com.example.sort;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by y on 2017/11/14.
+ */
+
+public class FindDuplicates {
+
+
+    /**
+     * Q:    1<= nums[n] <=n  找出重复元素
+     * A1:   自己做的话是考虑for双循环，low啊
+     *
+     * A2：  答案的话 我理解的是翻牌算法，根据(数组的值-1)当做数组下标，
+     *       根据这个下标对数组的值取负，判断是否负数，就可以判断值是不是重复
+     *       todo 注:遗留问题 当某个值是负数的时候，这时候去的数组下标是 负值-1的绝对值 这里没明白
+     *
+     */
+
+    public static List<Integer> findDuplicates(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] < 0) {
+                res.add(Math.abs(index + 1));
+            }
+            nums[index] = -nums[index];
+        }
+        return res;
+    }
+
+
+}
